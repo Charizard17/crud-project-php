b073dda8ffe5d93231b78ede36bf845824615bd9
esdu <esdu@Esads-MBP.mshome.net>
2020-09-09 11:56:55 +0200

    design process finished

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD App</title>
+    <!-- Create, Read, Update, Delete -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body>
@@ -36,6 +37,65 @@
         </form>
     </nav>
 
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <h3 class="tect-center text-dark mt-2">Advanced CRUD App Using PHP & MYSQLi Prepared Statement (Object Oriented)</h3>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <h3 class="text-center text-info">App Record</h3>
+                <form action="#" method="post" enctype="multipart/form-data">
+                <div class="form-group">
+                    <input type="text" name="name" class="form-control" placeholder="Enter name" required>
+                </div>
+                <div class="form-group">
+                    <input type="text" name="email" class="form-control" placeholder="Enter e-mail" required>
+                </div>
+                <div class="form-group">
+                    <input type="tel" name="phone" class="form-control" placeholder="Enter phone" required>
+                </div>
+                <div class="form-group">
+                    <input type="file" name="image" class="custom-file">
+                </div>
+                <div class="form-group">
+                    <input type="submit" name="add" class="btn btn-primary btn-block" value="Add Record">
+                </div>
+                </form>
+            </div>
+            <div class="col-md-8">
+                <h3 class="text-center text-info">Records Present In The Database</h3>
+                <table class="table table-hover">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Image</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td><img src="" width="25"></td>
+                        <td>John Doe</td>
+                        <td>doe@gmail.com</td>
+                        <td>0123456789</td>
+                        <td>
+                            <a href="#" class="badge badge-primary p-2 mb-1">Details</a> |
+                            <a href="#" class="badge badge-danger p-2 mb-1">Delete</a> |
+                            <a href="#" class="badge badge-success p-2 mb-1">Edit</a> |
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
     
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
